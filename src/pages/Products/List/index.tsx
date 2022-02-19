f0c4b899033a3ca146673b30c12f4cd6cec331c3
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-02-18 22:51:20 -0300

    implementado react query na lista de produtos

--- src/pages/Products/List/index.tsx
+++ src/pages/Products/List/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import { IProduct } from "../../../types/IProduct";
+import { useQuery } from "react-query";
 
 const fetchProducts = () => {
   return Axios.get(`http://localhost:3333/products`).then(
@@ -13,20 +14,25 @@ type ProductListProps = {
 };
 
 export const ProductList = ({ onProductDetail }: ProductListProps) => {
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  useEffect(() => {
-    setIsLoading(true);
+  const { data: products, isLoading } = useQuery<IProduct[]>(["products"], () =>
     fetchProducts()
-      .then((data) => {
-        setProducts(data);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
-  if (isLoading) {
+  );
+
+  // const [products, setProducts] = useState<IProduct[]>([]);
+  // const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetchProducts()
+  //     .then((data) => {
+  //       setProducts(data);
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  if (isLoading || !products) {
     return <h1>Loading products list...</h1>;
   }
   return (
